387ac41b9b5860789455b124096d8e82bd40302f
Chan-Pumpkin <chanpumpkin@gmail.com>
2021-05-01 21:41:44 +0900

    chanEgovBoard create

---
+++ src/main/java/egovframework/board/web/BoardController.java
@@ -0,0 +1,16 @@
+package egovframework.board.web;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+import egovframework.board.service.BoardService;
+
+@Controller
+public class BoardController {
+
+	@Resource
+	private BoardService boardService;
+	
+	
+}
