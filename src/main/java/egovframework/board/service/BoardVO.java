e51e1e3131591eed890abe441a0366c29c64a341
Chan-Pumpkin <chanpumpkin@gmail.com>
2021-05-03 23:35:53 +0900

    chanEgovBoard 2021-05-03
    BoardVO update

--- src/main/java/egovframework/board/service/BoardVO.java
+++ src/main/java/egovframework/board/service/BoardVO.java
@@ -1,5 +1,61 @@
 package egovframework.board.service;
 
 public class BoardVO {
+	
+	/** 게시물 아이디 **/
+	private String boardidx;
+	/** 게시물 제목 **/
+	private String title;
+	/** 게시물 내용**/
+	private String contents;
+	/** 작성자 ID **/
+	private String writerId;
+	/** 작성자 이름 **/
+	private String writerNm;
+	/** 작성 날짜 **/
+	private String indate;
+	
+	
+	public String getBoardidx() {
+		return boardidx;
+	}
+	public void setBoardidx(String boardidx) {
+		this.boardidx = boardidx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	public String getWriterNm() {
+		return writerNm;
+	}
+	public void setWriterNm(String writerNm) {
+		this.writerNm = writerNm;
+	}
+	public String getIndate() {
+		return indate;
+	}
+	public void setIndate(String indate) {
+		this.indate = indate;
+	}
+	
+	
+	
+	
+	
 
 }
